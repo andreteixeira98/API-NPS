1dad0a56ff0be3845380c6447c7e58adea400eb5
andreteixeira98 <andreteixeira@alu.ufc.br>
2021-03-16 19:46:34 -0300

    aula 1 nodejs

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express(); 
+
+app.listen(3333, ()=>console.log('server is running!'));
+
+app.get('/',(req,res)=>{
+    res.json({message:'lista de produtos'});
+});
+
+app.post('/',(req, res)=>{
+    res.json({message:'cadastro realizado com sucesso'});
+});
